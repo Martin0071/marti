1df5e1dc25b38c4a24f09997991561d39fbb0d29
Martin Petrov <hacker1121@abv.bg>
2021-11-22 18:50:38 +0200

    I think I did it

---
+++ source/repos/WebApplication6/Controllers/ReservationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication6.Models;
+using WebApplication6.Services;
+
+namespace WebApplication6.Controllers
+{
+	public class ReservationController : Controller
+	{
+		public IActionResult Index()
+		{
+
+			return View();
+		}
+
+		public IActionResult ProcessReserve(UserReserve flight)
+		{
+			SecurityService security = new SecurityService();
+			if (security.FlightIsRegged(flight))
+			{
+				return View("ReserveSucess", flight);
+			}
+			else
+			{
+				Console.WriteLine("Fuck you");
+				return View("Бум");
+
+			}
+		}
+	}
+}
+	
---
+++ source/repos/WebApplication6/Models/AdminView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication6.Models
+{
+	public class AdminView
+	{
+		public int Id { get; set; }
+		public string first_name { get; set; }
+		public string second_name { get; set; }
+		public string family_name { get; set; }
+		public string type { get; set; }
+		public DateTime date_landing { get; set; }
+		public DateTime date_leaving { get; set; }
+
+	}
+}
