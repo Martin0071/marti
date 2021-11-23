d8a9a160647a21a24d6f5213d86d6cdf59bfff21
Martin Petrov <hacker1121@abv.bg>
2021-11-24 01:25:22 +0200

    Commit

---
+++ source/repos/WebApplication6/Controllers/RegisterController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RazorPagesMovie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication6.Services;
+
+namespace WebApplication6.Controllers
+{
+	public class RegisterController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult ProcessRegister(AdminRegModel adminreg)
+		{
+			SecurityService security = new SecurityService();
+			if (security.isRegged(adminreg)) { 
+			return View("RegisterSucess", adminreg);
+		}
+			else
+			{
+				return View("RegisterFailure", adminreg);
+			}
+		}
+	}
+}
